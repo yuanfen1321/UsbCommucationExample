14c5a213d7c52b46b2d21e0ea4e9b7dc90fe394d
lijh <lijh@giec.cn>
2017-08-21 10:58:11 +0800

    change buffer size constant to much larger

--- usbcon/src/com/example/usbcon/UsbUtils/Constants.java
+++ usbcon/src/com/example/usbcon/UsbUtils/Constants.java
@@ -2,7 +2,12 @@ package com.example.usbcon.UsbUtils;
 
 public class Constants {
     public static final int USB_TIMEOUT_IN_MS = 100;
-    public static final int BUFFER_SIZE_IN_BYTES = 256;
+    /**
+     * it is said that in linux  limit of the buffer pipe size is 32769 bytes, 
+     * but from my test, the buffer size can be much larger.
+     * @see https://stackoverflow.com/questions/10889461/android-usb-host-api-bulk-transfer-buffer-size
+     */    
+    public static final int BUFFER_SIZE_IN_BYTES = 16384;
     public static final String DISCONNECTED = "disconnectedCmd";
     public static final int ACCESSORY_MAX_BUFFER_SIZE = 16384;
     
